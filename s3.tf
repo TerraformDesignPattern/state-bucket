782b58cdef1daa393c2b15e93e97ef35c1bfe8f3
Jonathon Brouse <jbrouse19@gmail.com>
2017-02-01 22:00:21 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "terraform-state-${var.aws_account}"
+  bucket = "${var.owner_name}-terraform-state-${var.aws_account}"
   acl = "private"
   versioning {
     enabled = true
