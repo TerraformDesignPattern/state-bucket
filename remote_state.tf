de59ac599c714be0ff490b0b59c7426c4568fc21
Jonathon Brouse <jbrouse19@gmail.com>
2017-02-01 21:50:21 -0500

    Update remote_state.tf

--- remote_state.tf
+++ remote_state.tf
@@ -2,7 +2,7 @@ resource "terraform_remote_state" "remote_state" {
   depends_on = ["aws_s3_bucket.s3_bucket"]
   backend = "s3"
   config {
-    bucket = "terraform-state-${var.aws_account}"
+    bucket = "${var.owner_name}-terraform-state-${var.aws_account}"
     key = "global/terraform-state-bucket/terraform.tfstate"
     region = "us-east-1"
   }
